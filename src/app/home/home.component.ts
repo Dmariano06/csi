fc65b93236fac0ac6a8301710a371f7fa0999eac
Dmariano06 <djilanimariano@outlook.fr>
2023-12-12 07:22:32 +0100

    commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -29,10 +29,10 @@ export class HomeComponent {
     {question:"Quelle est la différence entre un site internet et une application web, et comment choisir la meilleure option pour mes besoins ?", answer:"Un site internet est idéal pour une présence en ligne statique, tandis qu'une application web offre une interactivité avancée. Le choix dépend de vos objectifs spécifiques. Nous pouvons vous guider pour prendre la meilleure décision en fonction de vos besoins."},
     {question:"Quel est le délai de création pour un site web ou une application personnalisée ?", answer:"Les délais de création varient en fonction de la complexité du projet. Cependant, nous travaillons efficacement pour respecter des échéanciers raisonnables. Une fois que nous aurons discuté de vos besoins spécifiques, nous pourrons vous fournir une estimation plus précise du délai de livraison."}
   ]
-  image2="../../assets/img/illustrations/Le_pape_de_Jsus_design_paintmodernworkinguxuidecoration3dvector_60f9559b-8337-4ef8-9cde-c9851e6b68a2 - Modifié.png";
+  /*image2="../../assets/img/illustrations/Le_pape_de_Jsus_design_paintmodernworkinguxuidecoration3dvector_60f9559b-8337-4ef8-9cde-c9851e6b68a2 - Modifié.png";
   image1="assets/img/Le_pape_de_Jsus_rocket_after_launch_with_smoke_below_modern_uxu_1183455e-054a-44a5-ad9a-fe422630038d.webp";
   image11="assets/img/Le_pape_de_Jsus_rocket_after_launch_with_smoke_below_modern_uxu_1183455e-054a-44a5-ad9a-fe422630038d.png";
   image3="../../assets/img/illustrations/Le_pape_de_Jsus_developpementmodernworkinguxuidecoration3dvecto_ee2908ff-4600-43ca-97f8-82cb4afeedfd - Modifié.png";
   image4 = "../../assets/img/illustrations/Le_pape_de_Jsus_modern_working_ux_ui_decoration_2d_vector_art_c_f27fa1ac-a22d-449c-91cb-da7f473d333c - Modifié.png";
-
+*/
 }
