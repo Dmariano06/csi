921b6181ed243d508597dfd643dbc84c27fdbd52
Dmariano06 <djilanimariano@outlook.fr>
2023-12-14 04:27:23 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,5 @@ import { Component, HostListener } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  isNavbarTransparent : boolean = true;
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const offset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-
-    if (offset > 1200) {
-      this.isNavbarTransparent = false;
-    } else {
-      this.isNavbarTransparent = true;
-    }
-  }
+ 
 }
