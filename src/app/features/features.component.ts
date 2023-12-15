3e33e02c780df979a1d58119d9df24eb75a38dd5
Dmariano06 <djilanimariano@outlook.fr>
2023-12-15 16:10:28 +0100

    commit

--- src/app/features/features.component.ts
+++ src/app/features/features.component.ts
@@ -9,7 +9,7 @@ export class FeaturesComponent {
 texts = [ {name:"Bienvenue chez CSI", name1:"votre partenaire de confiance pour des solutions web robustes et performantes."}];
 services = [{name:'Application web', name1:"Votre entreprise est unique, et votre présence en ligne devrait l'être aussi. Notre service d'application web sur mesure offre une personnalisation inégalée par rapport aux sites vitrines traditionnels"},{name:'ATOUTS', name1:" Là où un site vitrine offre une présentation standard, notre application web sur mesure va bien au-delà, offrant une expérience unique à vos visiteurs.",img:""}];
 webapps = [{name:'Title', name1:"Imaginez un site qui évolue avec votre entreprise, qui s'adapte à vos besoins spécifiques, et qui reflète véritablement votre identité."},{name:'Title', name1:"Vous ne serez pas limité par des modèles prédéfinis. Chaque aspect de votre application est pensé et adapté à vos exigences uniques."}];
-others=[{name:'Title', name1:'description',img:"assets/img/illustrations/features4.png"},{name:'Title', name1:'description',img:"assets/img/illustrations/features5.png"},{name:'Title', name1:'description',img:"assets/img/illustrations/features6.png"}]
+others=[{name:'Title', name1:'description',img:"assets/img/illustrations/feature5.png"},{name:'Title', name1:'description',img:"assets/img/illustrations/feature2.png"},{name:'Title', name1:'description',img:"assets/img/illustrations/features.png"}]
 
 isTextTransparent : boolean = true;
   @HostListener('window:scroll', [])
