ea73b0545075ab3e691790b215062ecfc3ff7c21
Dmariano06 <djilanimariano@outlook.fr>
2023-12-18 17:11:43 +0100

    ccomit

--- src/app/features/features.component.ts
+++ src/app/features/features.component.ts
@@ -10,16 +10,16 @@ import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
       state('in', style({transform:'translateX(0%)', opacity: 1})),
       transition('void => *', [
         style({ transform:'translateX(150%)',opacity: 0 }),
-        animate('1.5s ease-in-out')
+        animate('1s ease-in-out')
       ]),
     ]),
   ],
 })
 export class FeaturesComponent {
 texts = [ {name:"Bienvenue chez CSI", name1:"votre partenaire de confiance pour des solutions web robustes et performantes."}];
-services = [{name:'Application web', name1:"Votre entreprise est unique, et votre présence en ligne devrait l'être aussi. Notre service d'application web sur mesure offre une personnalisation inégalée par rapport aux sites vitrines traditionnels"},{name:'ATOUTS', name1:" Là où un site vitrine offre une présentation standard, notre application web sur mesure va bien au-delà, offrant une expérience unique à vos visiteurs.",img:""}];
-webapps = [{name:'Title', name1:"Imaginez un site qui évolue avec votre entreprise, qui s'adapte à vos besoins spécifiques, et qui reflète véritablement votre identité."},{name:'Title', name1:"Vous ne serez pas limité par des modèles prédéfinis. Chaque aspect de votre application est pensé et adapté à vos exigences uniques."}];
-others=[{name:'Title', name1:'description',img:"assets/img/illustrations/feature5.png"},{name:'Title', name1:'description',img:"assets/img/illustrations/feature2.png"},{name:'Title', name1:'description',img:"assets/img/illustrations/features.png"}]
+services = [{name:'Quel site pour vous?', name1:"Chaque solution a ses avantages distincts, et notre équipe est prête à vous guider dans le choix qui correspond le mieux à vos objectifs, en mettant en avant les forces de chaque approche pour garantir le succès de votre projet"},{name:'Site Web Statique', name1:"Principalement axé sur la présentation d'informations et la représentation de l'entreprise en ligne. Il vise à informer les visiteurs sur les produits ou services.",img:""},];
+webapps = [{name:'Application web', name1:"Orientée vers l'interaction et l'engagement des utilisateurs. Elle offre une expérience plus dynamique et souvent personnalisée, mettant l'accent sur l'interaction utilisateur."},{name:'E-Commerce', name1:"Centré sur la vente en ligne de produits ou services. L'accent est mis sur la création d'une expérience d'achat fluide et sécurisée."}];
+others=[{name:'Site Vitrine - Impact Visuel, Coût Abordable', name1:"Création d'une première impression mémorable en toute simplicité. Optez pour la rapidité et la simplicité avec des designs élégants qui captent l'essence de votre entreprise dès le premier regard. Un moyen efficace d'être en ligne rapidement sans sacrifier l'esthétique.",img:"assets/img/illustrations/feature5.png", subtitle:"ShowCaseHub"},{name:'Applications Web - Puissance et Adaptabilité', name1:'Pour ceux qui cherchent à se démarquer avec une expérience utilisateur totalement personnalisée, nos applications web avec Angular sont la solution idéale. Bien que le processus puisse être plus complexe, chaque étape est minutieusement orchestrée pour répondre à vos besoins spécifiques. La personnalisation à 100% garantit que votre application reflète exactement votre vision, offrant une expérience utilisateur unique et sur mesure.',img:"assets/img/illustrations/feature2.png", subtitle:"CraftedWebSite"},{name:'E-commerce - Vendez en Ligne avec Style', name1:"Commerce en ligne sans compromis. Pour les sites e-commerce, nous proposons une solution qui allie rapidité de conception avec Bootstrap Studio et une technologie comme Rebflo pour maximiser la performance. Vous bénéficiez d'une interface intuitive et d'une gestion simplifiée des produits, créant ainsi une expérience d'achat en ligne sans compromis entre la facilité et la performance.",img:"assets/img/illustrations/features.png", subtitle:"CommerceDigitalXcellence"}]
 
 isTextTransparent : boolean = true;
   @HostListener('window:scroll', [])
@@ -38,7 +38,7 @@ isTextTransparent : boolean = true;
   onWindowSrcroll() {
     const scrollPosition = window.scrollY;
     // Ajustez la valeur de défilement à partir de laquelle l'image doit apparaître
-    const triggerPosition = 100;
+    const triggerPosition =300;
 
     if (scrollPosition > triggerPosition) {
       this.animationState = 'in';
