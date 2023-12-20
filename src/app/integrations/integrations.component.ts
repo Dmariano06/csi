d5d5a55566a51a348e49da18fa7927391069b11a
Dmariano06 <djilanimariano@outlook.fr>
2023-12-20 23:18:18 +0100

    commit

--- src/app/integrations/integrations.component.ts
+++ src/app/integrations/integrations.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-integrations',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './integrations.component.scss'
 })
 export class IntegrationsComponent {
+  constructor(private el: ElementRef) {}
 
+  scrollParallax(event: WheelEvent, section: number): void {
+    event.preventDefault(); // Empêche le défilement par défaut
+
+    const deltaY = event.deltaY;
+    const nextSection = section + (deltaY > 0 ? 1 : -1);
+
+    if (nextSection > 0 && nextSection <= 3) {
+      console.log(nextSection);
+      console.log(deltaY);
+      const nextElement = document.querySelector(`.parallax${nextSection}`) as HTMLElement;
+      if (nextElement) {
+        nextElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    }
+  }
+  
 }
--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -10,9 +10,94 @@ export class ServicesComponent {
 ,{name:"Nombres de pages sur demande"},{}
 ]
 uls=[{name:"Porte d'Entrée Digitale"},];
-  services = [ {name:"ShowCaseHub", subtitle:"Site Vitrine - Impact Visuel, Coût Abordable", name1title:"Conception Web & Contenu", name1:"Porte d'Entrée Digitale", name2title:"Référencement & Visibilité" ,name2:"Responsive et polyvalent ",name3title:"Accompagnement & Suivi:", name3:"Contenu optimisé pour votre activité et performant", name4:"Référencement naturel SEO contrôlé",name5:"Fiche Google My Business", name6:"Indice de perofrmance Google controlé", name7:"Référencement local contrôlé", name8:"Hébergement inclus et stockage illimité",name9:"Contact Disponible 7j/7",name10:"Réponse sous 12h",name11:"Actualisations par e-mail", name12:"Point/Bilan tout les mois"},
+ /* services = [ {name:"ShowCaseHub", subtitle:"Site Vitrine - Impact Visuel, Coût Abordable", name1title:"Conception Web & Contenu", name1:"Porte d'Entrée Digitale", name2title:"Référencement & Visibilité" ,name2:"Responsive et polyvalent",name3title:"Accompagnement & Suivi:", name3:"Contenu optimisé pour votre activité et performant", name4:"Référencement naturel SEO contrôlé",name5:"Fiche Google My Business", name6:"Indice de perofrmance Google controlé", name7:"Référencement local contrôlé", name8:"Hébergement inclus et stockage illimité",name9:"Contact Disponible 7j/7",name10:"Réponse sous 12h",name11:"Actualisations par e-mail", name12:"Point/Bilan tout les mois"},
   {name:"CraftedWebSite", subtitle:"Applications Web - Puissance et Adaptabilité",name1title:"Conception Web & Contenu", name1:"Porte d'entrée Digitale ", name2title:"Référencement & Visibilité",name2:"Des fonctionnalités sur mesure", name3title:"Évolutivité Potentielle", name3:"Des solutions conçues pour s'adapter et évoluer", nom3:"Nombre de pages sur demande (4 comprises)", nom4:"Optimisation intégrale des contenus ( média, développement, fonctionnalités...)", name6:"Un site entièrement personnalisable"},
    {name:"CommerceDigitalXcellence",subtitle:"E-commerce - Vendez en Ligne avec Style", name1title:"Solutions de Paiement Sécurisées",name1:"Assurez la confiance de vos clients avec des transactions sécurisées.", name2title:"Solutions de Paiement Sécurisées", name2:"Instaurez la confiance avec des solutions de paiement sûres et conformes aux normes de sécurité.",name3title:"Optimisation de l'Expérience", name3:"Des processus d'achat fluides et intuitifs pour maximiser les conversions visiteurs."},
-  ]
- 
+  ]*/
+  services = [
+    {
+      title: 'ShowCaseHub',
+      subtitle: 'Site Vitrine - Impact Visuel, Coût Abordable',
+      price: '$499',
+      bestValue: false,
+      bgClass: 'bg',
+      designTitle: 'Conception Web & Contenu',
+      designFeatures: [
+        "Porte d'entrée Digital",
+        'UI/UX Design',
+        'Responsive et polyvalent',
+      ],
+      seoTitle: 'SEO Features',
+      seoFeatures: [
+        'Référencement naturel SEO',
+        'Fiche Google My Business',
+        'Indice de perofrmance Google',
+        'Référencement local'
+      ],
+      supportTitle: 'Accompagnement & Suivi',
+      supportFeatures: [
+        '24/7 Support',
+        'Bug Fixes',
+        'Security Updates',
+        'Content Updates'
+      ]
+    },
+    {
+      title: 'CraftedWebSite',
+      subtitle: 'Applications Web - Puissance et Adaptabilité',
+      price: '$499',
+      bestValue: false,
+      bgClass: '1',
+      designTitle: 'Design Features',
+      designFeatures: [
+        "Porte d'entrée Digitale",
+        'UI/UX Design',
+        'Responsive et polyvalent',
+        'Un site entièrement personnalisable (sur mesure)',
+        'Nombre de pages sur demande'
+      ],
+      seoTitle: 'SEO Features',
+      seoFeatures: [
+        'On-page SEO',
+        'Keyword Research',
+        'Link Building',
+        'Analytics Setup'
+      ],
+      supportTitle: 'Support Features',
+      supportFeatures: [
+        '24/7 Support',
+        'Bug Fixes',
+        'Security Updates',
+        'Content Updates'
+      ]
+    },
+    {
+      title: 'Web Design',
+      subtitle: 'Modern and responsive web design',
+      price: '$499',
+      bestValue: false,
+      bgClass: '2',
+      designTitle: 'Design Features',
+      designFeatures: [
+        'Responsive Design',
+        'UI/UX Design',
+        'Custom Graphics',
+        'Animations'
+      ],
+      seoTitle: 'SEO Features',
+      seoFeatures: [
+        'On-page SEO',
+        'Keyword Research',
+        'Link Building',
+        'Analytics Setup'
+      ],
+      supportTitle: 'Support Features',
+      supportFeatures: [
+        '24/7 Support',
+        'Bug Fixes',
+        'Security Updates',
+        'Content Updates'
+      ]
+    }
+  ];
 }
